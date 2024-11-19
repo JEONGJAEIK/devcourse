975f8db4d7a4b85e32a4084d62424e450fafdd08
JEONGJAEIK <wodlr1207@naver.com>
2024-11-19 15:09:48 +0900

    명언 4단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String cmd;
-
+        int count = 0;
         System.out.println("== 명언 앱 ==");
         do {
             System.out.print("명령) ");
@@ -19,6 +19,8 @@ public class Main {
                 ;scanner.nextLine();
                 System.out.print("작가 : ");
                 scanner.nextLine();
+                count += 1;
+                System.out.println(count + "번 명언이 등록되었습니다");
             }
 
         } while (!Objects.equals(cmd, "종료"));
