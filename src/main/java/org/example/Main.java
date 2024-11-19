cbeeb69578150e6747d0786e3a72105af3646814
JEONGJAEIK <wodlr1207@naver.com>
2024-11-19 16:46:52 +0900

    명언 7단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -9,11 +9,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] wise_sayings = new String[10];
-        String[] authors = new String[10];
-        int[] numbers = new int[10];
+        ArrayList<String> wise_sayings = new ArrayList<>();
+        ArrayList<String> authors = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
+
         String cmd;
-        int count = 0;
+        int number = 1;
         System.out.println("== 명언 앱 ==");
 
         do {
@@ -22,26 +23,36 @@ public class Main {
 
             if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                wise_sayings[count] = scanner.nextLine();
+                wise_sayings.add(scanner.nextLine());
                 System.out.print("작가 : ");
-                authors[count] = scanner.nextLine();
-                numbers[count] = count += 1;
-                System.out.println(count + "번 명언이 등록되었습니다");
+                authors.add(scanner.nextLine());
+                numbers.add(number);
+                System.out.println(number + "번 명언이 등록되었습니다.");
+                number += 1;
 
             } else if (cmd.equals("목록")) {
-                for (int i = count - 1; i > - 1; i--) {
-                    System.out.println(numbers[i] + " / " + authors[i] + " / " + wise_sayings[i]);
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (int i = numbers.size() - 1; i > - 1; i--) {
+                    System.out.println(numbers.get(i) + " / " + authors.get(i) + " / " + wise_sayings.get(i));
                 }
             } else if (cmd.contains("삭제")) {
                 char delete_number_char = (char) cmd.charAt(6);
                 int delete_number = Character.getNumericValue(delete_number_char);
-                wise_sayings[delete_number - 1] = null;
-                authors[delete_number - 1] = null;
-                numbers[delete_number - 1] = 0;
-                System.out.println(delete_number + "번 명언이 삭제되었습니다");
+                try {
+                    wise_sayings.remove(numbers.indexOf(delete_number));
+                    authors.remove(numbers.indexOf(delete_number));
+                    numbers.remove((Integer) delete_number);
+                    System.out.println(delete_number + "번 명언이 삭제되었습니다.");
+                } catch (Exception e1) {
+                    System.out.println(delete_number + "번 명언은 존재하지 않습니다.");
+                }
 
             }
 
         } while (!Objects.equals(cmd, "종료"));
     }
 }
+
+
+
