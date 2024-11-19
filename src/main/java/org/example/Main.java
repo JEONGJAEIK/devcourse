ad95f87f44f674ed48929c693d42d0a269b60f3f
JEONGJAEIK <wodlr1207@naver.com>
2024-11-19 15:37:45 +0900

    명언 5단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -8,19 +9,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] wise_sayings = new String[10];
+        String[] authors = new String[10];
+        int[] numbers = new int[10];
         String cmd;
         int count = 0;
         System.out.println("== 명언 앱 ==");
+
         do {
             System.out.print("명령) ");
             cmd = scanner.nextLine();
-            if (Objects.equals(cmd, "등록")) {
-                System.out.print("명언 : ")
-                ;scanner.nextLine();
+
+            if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                wise_sayings[count] = scanner.nextLine();
                 System.out.print("작가 : ");
-                scanner.nextLine();
-                count += 1;
+                authors[count] = scanner.nextLine();
+                numbers[count] = count += 1;
                 System.out.println(count + "번 명언이 등록되었습니다");
+
+            } else if (cmd.equals("목록")) {
+                for (int i = count - 1; i > - 1; i--) {
+                    System.out.println(numbers[i] + " / " + authors[i] + " / " + wise_sayings[i]);
+                }
             }
 
         } while (!Objects.equals(cmd, "종료"));
